c6106b2bf2041803a64805dff5f7ba3586b96648
Mila Zhao <bz82@duke.edu>
2021-09-24 13:01:51 -0400

    changed 8:1 mux to behavioral verilog

--- Recitation5-BehavioralALU/mux_8to1_32b.v
+++ Recitation5-BehavioralALU/mux_8to1_32b.v
@@ -4,12 +4,16 @@ module mux_8to1_32b(out, in0, in1, in2, in3, in4, in5, in6, in7, sel);
 	input [2:0] sel;
 	output [31:0]out;
 	
-	wire [31:0]top_result, bottom_result;
-	
-	mux_4to1_32b mux_4to1_top(top_result, in0, in1, in2, in3, sel[1:0]);
-	mux_4to1_32b mux_4to1_bottom(bottom_result, in4, in5, in6, in7, sel[1:0]);
-	
-	mux_2to1_32b mux_2to1_result(out, top_result, bottom_result, sel[2]);
-	
-	
+	always @(in0, in1, in2, in3, in4, in5, in6, in7, sel) begin
+		case(sel):
+		3'd0: out = in0;
+		3'd1: out = in1;
+		3'd2: out = in2;
+		3'd3: out = in3;
+		3'd4: out = in4;
+		3'd5: out = in5;
+		3'd6: out = in6;
+		3'd7: out = in7;
+		endcase
+	end
 endmodule
