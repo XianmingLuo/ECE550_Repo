01935d5d7ff4625eb6b5ee8edb1e87b79cfe63ac
XianmingLuo <xianming0520@gmail.com>
2021-09-24 13:12:57 -0400

    Debug 8:1 mux and make 4:1 mux into behavioral verilog

--- Recitation5-BehavioralALU/mux_4to1_32b.v
+++ Recitation5-BehavioralALU/mux_4to1_32b.v
@@ -2,13 +2,14 @@ module mux_4to1_32b(out, in0, in1, in2, in3, sel);
 	
 	input [31:0] in0, in1, in2, in3;
 	input [1:0] sel;
-	output [31:0]out;
+	output reg [31:0] out;
 	
-	wire [31:0]top_result, bottom_result;
-	
-	mux_2to1_32b mux_2to1_top(top_result, in0, in1, sel[0]);
-	mux_2to1_32b mux_2to1_bottom(bottom_result, in2, in3, sel[0]);
-	
-	mux_2to1_32b mux_2to1_result(out, top_result, bottom_result, sel[1]);
-	
+	always @(in0, in1, in2, in3, sel) begin
+		case(sel)
+			2'd0: out <= in0;
+			2'd1: out <= in1;
+			2'd2: out <= in2;
+			2'd3: out <= in3;
+		endcase
+	end
 endmodule
--- Recitation5-BehavioralALU/mux_8to1_32b.v
+++ Recitation5-BehavioralALU/mux_8to1_32b.v
@@ -2,18 +2,18 @@ module mux_8to1_32b(out, in0, in1, in2, in3, in4, in5, in6, in7, sel);
 	
 	input [31:0] in0, in1, in2, in3, in4, in5, in6, in7;
 	input [2:0] sel;
-	output [31:0]out;
+	output reg [31:0] out;
 	
 	always @(in0, in1, in2, in3, in4, in5, in6, in7, sel) begin
-		case(sel):
-		3'd0: out = in0;
-		3'd1: out = in1;
-		3'd2: out = in2;
-		3'd3: out = in3;
-		3'd4: out = in4;
-		3'd5: out = in5;
-		3'd6: out = in6;
-		3'd7: out = in7;
+		case(sel)
+			3'd0: out <= in0;
+			3'd1: out <= in1;
+			3'd2: out <= in2;
+			3'd3: out <= in3;
+			3'd4: out <= in4;
+			3'd5: out <= in5;
+			3'd6: out <= in6;
+			3'd7: out <= in7;
 		endcase
 	end
 endmodule
