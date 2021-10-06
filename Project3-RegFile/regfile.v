b5483c53853d8880c60dc8497b18e5dc0080cc6f
Mila Zhao <bz82@duke.edu>
2021-10-06 19:42:23 -0400

    wrote regfile.v, but has error: output(read) doesnt match with input(write)

--- Project3-RegFile/regfile.v
+++ Project3-RegFile/regfile.v
@@ -14,4 +14,46 @@ module regfile (
 
    /* YOUR CODE HERE */
 	
+	// wires for decoder's [31:0] outputs
+	wire [31:0] dcd_outwires_wrt, dcd_outwires_read_A, dcd_outwires_read_B;
+	
+	// 3 Decoders - module decoder_5to32( [31:0]out, [4:0]in, en);
+	decoder_5to32 dcd_write(dcd_outwires_wrt, ctrl_writeReg, 1'b1);    // for writing
+	decoder_5to32 dcd_readA(dcd_outwires_read_A, ctrl_readRegA, 1'b1); // for reading A
+	decoder_5to32 dcd_readB(dcd_outwires_read_B, ctrl_readRegB, 1'b1); // for reading B
+	
+	
+	/* !!! 
+	// how to make 32 of these wires??? ---- I put them into the generate loop, is it right?
+	wire [31:0] q; 
+	wire wr_en_wires; */
+	
+	
+	// generate 32 times
+	genvar i;
+	generate
+		for(i = 0; i < 32; i = i+1) begin: name
+			// 32 set of wires using generate loop
+			wire [31:0] q; 
+			wire wr_en_wire;
+			
+			// 1. writing port: connection between decoder and register 
+			and and_0(wr_en_wire, dcd_outwires_wrt[i], ctrl_writeEnable); 
+			
+			// 2. anything connects to the 32 registers 
+			//    ---- module register_32b( [31:0]q, [31:0]d, clk, en, clr);
+			register_32b reg_0(q, data_writeReg, clock, wr_en_wire, ctrl_reset); //clr = ctrl_reset??
+			
+			// 3. Reading A port: connection between register and output 
+			//    (---- e.g. bufif1  b1(out, in, oe);)
+			assign data_readRegA = dcd_outwires_read_A[i]? q : 1'bz;
+				
+			// 4. Reading B port: connection between register and output  
+			assign data_readRegB = dcd_outwires_read_B[i]? q : 1'bz;
+			
+		end
+	endgenerate
+	
+	
+	
 endmodule
---
+++ Project3-RegFile/simulation/modelsim/regfile_run_msim_rtl_verilog.do.bak2
@@ -0,0 +1,20 @@
+transcript on
+if {[file exists rtl_work]} {
+	vdel -lib rtl_work -all
+}
+vlib rtl_work
+vmap work rtl_work
+
+vlog -vlog01compat -work work +incdir+C:/Users/bz82/Documents/ECE\ 550/ECE550_Repo/Project3-RegFile {C:/Users/bz82/Documents/ECE 550/ECE550_Repo/Project3-RegFile/decoder_5to32.v}
+vlog -vlog01compat -work work +incdir+C:/Users/bz82/Documents/ECE\ 550/ECE550_Repo/Project3-RegFile {C:/Users/bz82/Documents/ECE 550/ECE550_Repo/Project3-RegFile/regfile.v}
+vlog -vlog01compat -work work +incdir+C:/Users/bz82/Documents/ECE\ 550/ECE550_Repo/Project3-RegFile {C:/Users/bz82/Documents/ECE 550/ECE550_Repo/Project3-RegFile/dffe_ref.v}
+vlog -vlog01compat -work work +incdir+C:/Users/bz82/Documents/ECE\ 550/ECE550_Repo/Project3-RegFile {C:/Users/bz82/Documents/ECE 550/ECE550_Repo/Project3-RegFile/register_32b.v}
+
+vlog -vlog01compat -work work +incdir+C:/Users/bz82/Documents/ECE\ 550/ECE550_Repo/Project3-RegFile {C:/Users/bz82/Documents/ECE 550/ECE550_Repo/Project3-RegFile/regfile_tb.v}
+
+vsim -t 1ps -L altera_ver -L lpm_ver -L sgate_ver -L altera_mf_ver -L altera_lnsim_ver -L cycloneive_ver -L rtl_work -L work -voptargs="+acc"  regfile_tb
+
+add wave *
+view structure
+view signals
+run -all
